f9271439d479aa07d75c671104b1ddecba4de7bf
Osokin Yaroslav <osyaros.2005@yandex.ru>
2024-10-14 11:47:23 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/rezume/',
   optimizeDeps: {
     esbuildOptions: {
       define: {
